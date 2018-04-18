41cb036b6732bab76a61006fd0247febb15e6bc6
ulag-india <ulag.india@gmail.com>
2018-04-19 00:46:39 +0530

    Test Message

--- src/com/sample/HelloWorld.java
+++ src/com/sample/HelloWorld.java
@@ -8,6 +8,6 @@ public class HelloWorld {
    }
 
    public void getMessage(){
-      System.out.println("Your Message : " + message);
+      System.out.println("Test Message : " + message);
    }
 }
